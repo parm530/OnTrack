a9f234a4dc162d8775e92864af2c982982bae7ad
sami4thewin <samiomarmohammed@gmail.com>
2016-11-16 15:31:52 -0500

    Added events to users association

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -62,6 +62,14 @@ class EventsController < ApplicationController
     end
   end
 
+  def complete
+    @event.status = true
+    respond_to do |format|
+      format.html { redirect_to events_url, notice: 'Event was successfully completed.' }
+      format.json { head :no_content }
+    end
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_event
